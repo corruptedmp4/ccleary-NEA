4b464415c7ce7180390d73ad87de4bde291c5f00
corruptedmp4 <s0037225@wyke.ac.uk>
2025-01-07 14:21:22 +0000

    stable, need to make the fuckin uhhhhhhhhhhhhhhhhh clickable build menu work

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js"
-import * as SkeletonUtils from "three/addons/utils/SkeletonUtils.js"
+
 let camera, scene, renderer, playerCube, previousPlayer, previousCamera, playerCubeHealthIndicator;
 let collidableMeshList = [];
 let enemyList = [];
